8be084790e833f405f839198831adb3c6a062242
sqwot <sqwotina@gmail.com>
2019-03-31 20:04:05 +0300

    Добавьте файлы проекта.

---
+++ ForStudentota/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForStudentota {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
